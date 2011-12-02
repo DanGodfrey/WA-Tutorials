6fbad70c4e52c647dc5d85cbf27c016a74a32c8e
dangodfrey <c.daniel.godfrey@gmail.com>
2011-12-01 21:05:56 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WaTutorial::Application.config.secret_token = 'f499bb278ffc88aba4b7eaf12f9620e7684fa9e77b0c0699eb32c50dbe013eae909e544ee22303ed43c51280c856eec20a0cba973aa377df227ae5eb006d7610'
